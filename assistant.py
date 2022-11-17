02999150eadbdb3943124f910cc47980656094d0
Arpit <74809468+arpitsengar99@users.noreply.github.com>
2022-11-16 22:05:21 -0800

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -58,10 +58,10 @@ while(1):
             
             # Using google to recognize audio
             MyText = r.recognize_google(audio2)
-            MyText = MyText.lower()
-     
-            
-            if MyText == 'play a song' or MyText == "mujhe ek gana sunna hai" :
+            MyText = MyText.lower().split(" ")
+            print(MyText)
+            if "song" in MyText:             
+            # if MyText == 'play a song' or MyText == "mujhe ek gana sunna hai" :
                                                     SpeakText('Okay what song would you like to hear?')
                                                     print("Okay what song would you like to hear?")
                                                     audio2 = r.listen(source2)            
@@ -82,63 +82,63 @@ while(1):
                                                     click(x=600, y= 380, button = 'left')
                                                     
                                     
-            elif MyText == "show my channel on youtube":
-                                                    SpeakText('Opening Youtube')
-                                                    print('Opening youtube')
-                                                    keyDown('win')
-                                                    press('r')
-                                                    keyUp('win')
-                                                    write('brave')
-                                                    press('enter', interval = 3)
-                                                    write('https://www.youtube.com/channel/UCDzWo3sdRD6LnQUETpFpP4g/videos')
-                                                    press('enter')            
+            # elif MyText == "show my channel on youtube":
+            #                                         SpeakText('Opening Youtube')
+            #                                         print('Opening youtube')
+            #                                         keyDown('win')
+            #                                         press('r')
+            #                                         keyUp('win')
+            #                                         write('brave')
+            #                                         press('enter', interval = 3)
+            #                                         write('https://www.youtube.com/channel/UCDzWo3sdRD6LnQUETpFpP4g/videos')
+            #                                         press('enter')            
                                               
-            elif MyText == "play that song":
-                                                    SpeakText('Now playing After Dark x Sweater Weather from youtube')
-                                                    print('Now playing After Dark x Sweater Weather from youtube')
-                                                    keyDown('win')
-                                                    press('r')
-                                                    keyUp('win')
-                                                    write('brave')
-                                                    press('enter', interval = 3)
-                                                    write('https://youtu.be/hvL-2TF9bLY')
-                                                    press('enter')           
+            # elif MyText == "play that song":
+            #                                         SpeakText('Now playing After Dark x Sweater Weather from youtube')
+            #                                         print('Now playing After Dark x Sweater Weather from youtube')
+            #                                         keyDown('win')
+            #                                         press('r')
+            #                                         keyUp('win')
+            #                                         write('brave')
+            #                                         press('enter', interval = 3)
+            #                                         write('https://youtu.be/hvL-2TF9bLY')
+            #                                         press('enter')           
 
-            elif MyText == "decrease volume" or MyText == "awaaz ghataen":
+            elif "decrease" in MyText:
                                                     SpeakText("Decreasing the volume by 20")
                                                     print("Volume decreased by 20")
                                                     for i in range(11):
                                                         press('volumedown')
 
-            elif MyText == "increase volume" or MyText == "awaaz badhaen":
+            elif "increase" in MyText:
                                                     SpeakText("Increasing the volume by 20")
                                                     print("Volume increased by 20")
                                                     for i in range(11):
                                                         press('volumeup')
 
-            elif MyText == "mute volume" or MyText == "awaaz ko band karen"  or MyText == "mute":
+            elif "mute" in MyText:
                                                     SpeakText("Volume muted")
                                                     print("Volume muted")
                                                     press('volumemute')
                                                 
-            elif MyText == "unmute volume" or MyText == "unmute" or MyText == "awaaz ko chalu karen":
+            elif "unmute" in MyText:
                                                     SpeakText("Volume unmuted")
                                                     print("Volume unmuted")
                                                     press('volumemute')
             
-            elif MyText == "home screen":                              
+            elif "home" in MyText:                              
                                                     keyDown('win')
                                                     press('d')
                                                     keyUp('win')
             
-            elif MyText == "switch tabs":
+            elif "switch" in MyText:
                                                     SpeakText("Switching tabs")
                                                     print("Tabs switched")
                                                     keyDown('alt')
                                                     press('tab')
                                                     keyUp('alt')
 
-            elif MyText == "create desktop" or MyText == "create a desktop":
+            elif "create" in MyText:
                                                     SpeakText("Creating a desktop")
                                                     print("Desktop created successfully")
                                                     keyDown('win')
@@ -146,7 +146,7 @@ while(1):
                                                     press('d')
                                                     keyUp('ctrl')
                                                     keyDown('win')
-            elif MyText == "close desktops"  or MyText == "close all desktops": 
+            elif "close" in MyText: 
                                                     for i in range(10):
                                                         keyDown('win')
                                                         keyDown('ctrl')
@@ -157,19 +157,19 @@ while(1):
                                                     print("All desktops deleted successfully")
                                                     
                                                     
-            elif MyText == "minimise":
+            elif "minimise" in MyText:
                                                     print("Window minimized")
                                                     keyDown('win')
                                                     press('down')
                                                     keyUp('win')
                                                     
-            elif MyText == "close application" :
+            elif "close" in MyText :
                                                     print("Application closed")
                                                     keyDown('alt')
                                                     press('f4')
                                                     keyUp('alt')
 
-            elif MyText == "left desktop" or MyText == "left screen":
+            elif "left" in MyText:
                                                     print("Desktop switched to the left")
                                                     keyDown('win')
                                                     keyDown('ctrl')
@@ -177,7 +177,7 @@ while(1):
                                                     keyUp('ctrl')
                                                     keyDown('win')
 
-            elif MyText == "show me the right desktop" or MyText == "right screen":
+            elif "right" in MyText:
                                                     print("Desktop switched to the right")
                                                     keyDown('win')
                                                     keyDown('ctrl')
@@ -185,16 +185,16 @@ while(1):
                                                     keyUp('ctrl')
                                                     keyDown('win')
 
-            elif MyText == "shut down":
+            elif "shutdown" in MyText or "shut" in MyText:
                                                     SpeakText("shutting down in")
                                                     SpeakText("5")
-                                                    write('.', interval = 0.25)
+                                                    write('.', interval = 0.15)
                                                     SpeakText("4")
-                                                    write('.', interval = 0.25)
+                                                    write('.', interval = 0.15)
                                                     SpeakText("3")
-                                                    write('.', interval = 0.25)
+                                                    write('.', interval = 0.15)
                                                     SpeakText("2")
-                                                    write('.', interval = 0.25)
+                                                    write('.', interval = 0.15)
                                                     SpeakText("1")
                                                     keyDown('win')
                                                     press('d')
@@ -204,7 +204,7 @@ while(1):
                                                     keyUp('alt')
                                                     press('enter')
                 
-            elif MyText == 'restart' or MyText == 'reboot':
+            elif "reboot" in MyText:
                                                     SpeakText("rebooting in")
                                                     SpeakText("5")
                                                     write('.', interval = 0.25)
@@ -224,7 +224,7 @@ while(1):
                                                     press('down')
                                                     press('enter')                               
             
-            elif MyText == 'refresh':
+            elif "refresh" in MyText:
                                                     keyDown('win')
                                                     press('d')
                                                     keyUp('win')
@@ -234,11 +234,23 @@ while(1):
                                                         keyUp('fn')
                                                     print("System refreshed")
 
-            elif MyText == 'maximum volume':            
+            elif "maximum" in MyText:            
                                                     for i in range(50):
                                                         press('volumeup')
                                                     print("Volume maximised")
-                                                                
+            elif "open" in MyText:
+                                                    SpeakText('Okay what would you like to open?')
+                                                    print('Okay what would you like to open?')
+                                                    # print("Okay what song would you like to hear?")
+                                                    audio2 = r.listen(source2)            
+                                                    # Using google to recognize audio
+                                                    MySong = r.recognize_google(audio2)
+                                                    MySong = MySong.lower()
+                                                    press("win")
+                                                    write(MySong, interval = 0.1)
+                                                    press("enter")
+                                                   
+                                                                                                      
             else:
                 print("Sorry i could not understand that ")
                 # SpeakText("Sorry i could not understand that")
